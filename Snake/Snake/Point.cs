ad021404491154ebac36119104d0f898c8c221eb
Midova <darya.demidova2011@gmail.com>
2016-08-23 14:03:04 +0300

    Point

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+	class Point
+	{
+		public int CoordinateX;
+		public int CoordinateY;
+		public char Symbol;
+
+		public Point()
+		{
+		}
+
+		public Point( int _x, int _y, char _sym)
+		{
+			CoordinateX = _x;
+			CoordinateY = _y;
+			Symbol = _sym;
+		}
+
+		public void Draw()
+		{
+			Console.SetCursorPosition(CoordinateX, CoordinateY);
+			Console.Write(Symbol);
+		}
+	}
+}
