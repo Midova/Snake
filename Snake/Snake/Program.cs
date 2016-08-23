2b77aa41a245f816e4019743f73252ef4910a049
Midova <darya.demidova2011@gmail.com>
2016-08-23 14:02:49 +0300

    Point

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,8 +10,13 @@ namespace Snake
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, world!");
-			Console.ReadLine();
+			Point p1 = new Point(1, 3, '*');
+			p1.Draw();
+
+			Point p2 = new Point(4, 5, '#');
+			p2.Draw();
+
+			Console.ReadKey();
 		}
 	}
 }
